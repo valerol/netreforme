5957fe4b7a54221734cd0330dd3ad0b2d815a903
valerol <valerol@ya.ru>
2016-04-26 17:51:20 +1000

    latest

--- wp-content/themes/netreforme/comments.php
+++ wp-content/themes/netreforme/comments.php
@@ -30,8 +30,6 @@ if ( post_password_required() ) {
 			?>
 		</h2>
 
-		<?php twentyfifteen_comment_nav(); ?>
-
 		<ol class="comment-list">
 			<?php
 				wp_list_comments( array(
@@ -41,8 +39,6 @@ if ( post_password_required() ) {
 				) );
 			?>
 		</ol><!-- .comment-list -->
-
-		<?php twentyfifteen_comment_nav(); ?>
 
 	<?php endif; // have_comments() ?>
 
