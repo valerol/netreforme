cee907f2d3c0de47b795297ea546d3a2c4c38d55
valerol <valerol@ya.ru>
2016-12-05 11:29:42 +1000

    Add files via upload
    right encoding

---
+++ footer.php
@@ -0,0 +1,29 @@
+		<div class="footer-container">
+			<footer>
+				<div class="donate">
+					<p>Сделайте пожертвование, чтобы поддержать наш блог и помочь всем быть в курсе событий</p>
+					<iframe frameborder="0" allowtransparency="true" scrolling="no" src="https://money.yandex.ru/embed/donate.xml?account=410011003238930&quickpay=donate&payment-type-choice=on&default-
+
+sum=100&targets=%D0%9F%D0%BE%D0%B4%D0%B4%D0%B5%D1%80%D0%B6%D0%BA%D0%B0+%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B0&project-name=NetReforme.org&project-site=netreforme.org&button-text=05&fio=on&mail=on&phone=on&successURL=" width="510" 
+
+height="105"></iframe>
+				</div>
+				<p class="copywright">© Гражданская инициатива за бесплатное образование, 2010–<?php echo date('Y'); ?></p>
+				<!--LiveInternet counter--><script type="text/javascript">document.write("<a href='//www.liveinternet.ru/click' target=_blank><img src='//counter.yadro.ru/hit?t25.1;r" + escape(document.referrer) + 
+
+((typeof(screen)=="undefined")?"":";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?screen.colorDepth:screen.pixelDepth)) + ";u" + escape(document.URL) + ";" + Math.random() + "' border=0 width=88 height=15 alt='' 
+
+title='LiveInternet: показано число посетителей за сегодня'><\/a>")</script><!--/LiveInternet-->
+			</footer>
+			<?php wp_footer(); ?>
+        </div>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+        <!-- Yandex.Metrika counter --><script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter4207894 = new Ya.Metrika({ id:4207894, clickmap:true, trackLinks:true, 
+
+accurateTrackBounce:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = 
+
+"https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img 
+
+src="https://mc.yandex.ru/watch/4207894" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
+    </body>
+</html>
